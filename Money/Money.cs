a658aad4f7c88598b81432a07feaf7e5771f8729
Vlad <vladvesinin676@gmail.com>
2025-04-01 19:03:47 +0500

    commit

---
+++ Money/Money.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money
+{
+    public class Money
+    {
+        public double Sum { get; set; }
+
+
+        public Money(double sum)
+        {
+            Sum = sum;
+        }
+
+
+        public static implicit operator Money(double sum)
+        {
+            return new Money(sum);
+        }
+
+
+        public static Money operator +(Money num1, Money num2)
+        {
+            return new Money(num1.Sum + num2.Sum);
+        }
+
+
+        public static Money operator +(Money num1, double num2)
+        {
+            Money res = num1 + num2;
+            return res;
+        }
+
+
+        public static Money operator -(Money num1, Money num2)
+        {
+            return new Money(num1.Sum - num2.Sum);
+        }
+
+
+        public static Money operator -(Money num1, double num2)
+        {
+            Money res = num1 - num2;
+            return res;
+        }
+
+
+
+        public static Money operator *(Money num1, int num2)
+        {
+            return new Money(num1.Sum * num2);
+        }
+
+        public static Money operator /(Money num1, int num2)
+        {
+            if (num2 == 0)
+            {
+                throw new DivideByZeroException("Деление на ноль запрещено!!");
+            }
+            return new Money(num1.Sum / num2);
+        }
+
+
+        public static Money operator ++(Money num)
+        {
+            return new Money(num.Sum + 0.1);
+        }
+
+        public static Money operator --(Money num)
+        {
+            return new Money(num.Sum - 0.1);
+        }
+
+
+        public static bool operator ==(Money num1, Money num2)
+        {
+            return num1.Sum == num2.Sum;
+        }
+
+
+        public static bool operator !=(Money num1, Money num2)
+        {
+            return num1.Sum != num2.Sum;
+        }
+
+
+        public static bool operator >(Money num1, Money num2)
+        {
+            return num1.Sum > num2.Sum;
+        }
+
+
+        public static bool operator <(Money num1, Money num2)
+        {
+            return num1.Sum < num2.Sum;
+        }
+    }
+}
---
+++ Money/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Money
+{
+    public class BankruptcyException : Exception
+    {
+        public BankruptcyException(string message) : base(message) { }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Money num1 = 100;
+            Money num2 = 200;
+
+
+            try
+            {
+                CheckResult(num1 + num2, "Сложение");
+                CheckResult(num2 - num1, "Вычитание");
+                CheckResult(num1 * 2, "Умножение");
+                CheckResult(num1 / 2, "Деление");
+                Console.WriteLine(num1 == num2);
+                Console.WriteLine(num1 != num2);
+                Console.WriteLine(num1 > num2);
+                Console.WriteLine(num1 < num2);
+                CheckResult(num1++, "+0.1");
+                CheckResult(num2++, "+0.1");
+                CheckResult(num1--, "-0.1");
+                CheckResult(num2--, "-0.1");
+            }
+            catch (BankruptcyException ex)
+            {
+                Console.WriteLine($"ERROR\n{ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR\n{ex.Message}");
+            }
+        }
+
+
+        static void CheckResult(Money result, string operation)
+        {
+            if (result.Sum < 0)
+            {
+                throw new BankruptcyException($"Вы обанкротились!! Результат операции {operation} меньше 0: {result.Sum}");
+            }
+            Console.WriteLine($"{operation}: {result.Sum}");
+        }
+    }
+}
